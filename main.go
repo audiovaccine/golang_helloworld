9038590408b13b2d635402e939bb63cfc196e0fb
audiovaccine <ykponia@gmail.com>
2023-07-31 23:56:33 +0300

    добавлен конвертер в цельсии

--- main.go
+++ main.go
@@ -3,5 +3,12 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello World!")
+	fmt.Print("Enter temperature in Celsium:")
+	var input float64
+	fmt.Scanf("%f", &input)
+	convertTemp(input)
+}
+func convertTemp(input float64) {
+	output := (input - 32) * 5 / 9
+	fmt.Print(output)
 }
